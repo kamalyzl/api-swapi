a1eef019dfdfd68c05700073e060f764f3826ca6
kamalyzl <kamaly.kazal@gmail.com>
2018-02-15 11:30:34 -0500

    Identacion

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@
 const container = document.getElementById('container');
 const modal = document.getElementById('body');
 
-
 getFetch();
 function getFetch() {
   // const url = `https://swapi.co/api/people/?search=${searchValue}`;
@@ -12,10 +11,11 @@ function getFetch() {
     const url = `https://swapi.co/api/people/${i}/`;
     fetch(url)
       .then((resp) => resp.json())
-      .then(function (data) {
-        // console.log(data);
+      .then(function(data) {
+        console.log(data);
         // console.log(data.name);
-        const characters = ` <figure class="images" id="containerImages" data-name="${data.name}">
+        const characters = ` <figure class="images" id="containerImages" data-name="${data.name}" data-mass="${data.mass}" data-hair_color="${data.hair_color}"
+        data-gender="${data.gender}" data-height="${data.height}" >
         <img class="image" data-target="#exampleModal" data-toggle="modal" src="https://starwars-visualguide.com/assets/img/characters/${i}.jpg" alt="${data.name}">
         <figcaption >${data.name}</figcaption>
       </figure>`;
@@ -33,6 +33,11 @@ $(document).on('click', 'figure', function() {
   let name = document.getElementById('modal-title');
   let modal = document.getElementById('body');
   name.innerHTML = el.data('name');
-  let text = 
+  let text = ` 
+  <p> Color:  ${el.data('hair_color')}</p>
+  <p> Peso:  ${el.data('mass')}</p>
+  <p> Genero:  ${el.data('gender')}</p>
+  <p> Altura:  ${el.data('height')}</p>`;
+  modal.innerHTML = text;
 });
 
